84a350416604e9a678d8a3878613580645ef5cc6
Jim <Jim@autio.com>
2022-09-09 17:44:59 +0800

    first commit

---
+++ src/HelloLeetcode.java
@@ -0,0 +1,9 @@
+/**
+ * @author Jin
+ * @Date 2022年09月2022/9/9日17:43
+ */
+public class HelloLeetcode {
+    public static void main(String[] args) {
+        System.out.println("Hello Leetcode!!");
+    }
+}
